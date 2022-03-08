b06ef2d5d7df333bafb2f92d8b36c84b71b0fe4e
Krutik48 <krutikmalani480@gmail.com>
2022-03-08 17:32:51 +0530

    comment cred

--- server/router/comment.js
+++ server/router/comment.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const commentController = require('../controller/comment_controller');
 
-router.post('/addcomment', commentController.create);
+router.post('/addcomment', commentController.createComment);
+router.put('/editcomment', commentController.editComment);
+router.delete('/deletecomment', commentController.deleteComment);
+router.get('/getcomment',commentController.getComment);
 
 module.exports = router;
