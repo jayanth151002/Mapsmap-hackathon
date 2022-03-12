ee4e92ceaceac6a6ea0ae56eed41b2ed36907dbc
THARUN.V <v.tharuniitm001@gmail.com>
2022-03-12 22:01:57 +0530

    QUESTIONRENDER

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,45 +1,54 @@
 import React from "react";
-import axios from 'axios'
 const QuestionPage = (props) => {
+  console.log(props.question);
   return (
     <div className="container mt-3">
-      <div className="display-1">{props.question.qnTitle}</div>
-      <div className="mt-3">{props.question.qnDescription}</div>
+      <div className="row">
+        <div className="col-1">
+          <div>
+            <button>
+              <i class="fa fa-arrow-up"></i>
+            </button>
+          </div>
+          <div className="mx-2">{props.question.data.question.vote}</div>
+          <div>
+            <button>
+              <i class="fa fa-arrow-down"></i>
+            </button>
+          </div>
+        </div>
+        <div className="h3 col-8 my-auto display-1">
+          {props.question.data.question.question}
+        </div>
+      </div>
+      <div className="mt-3">{props.question.data.question.body}</div>
       <div className="display-4 my-3">Suggestions</div>
       <div>
-        {props.question.answers.map((answer) => {
+        {/* {props.question.data.question.comments.map((answer) => {
           return (
             <div className="mt-2">
               <div className="row">
                 <div className="col-1">
                   <div>
-                    <button
-                      onClick={() => {
-                        props.Update_UpVote(props.question.id, answer.id);
-                      }}
-                    >
+                    <button>
                       <i class="fa fa-arrow-up"></i>
                     </button>
                   </div>
-                  <div className="mx-2">{answer.VoteCount}</div>
+                  <div className="mx-2"></div>
                   <div>
-                    <button
-                      onClick={() => {
-                        props.Update_DownVote(props.question.id, answer.id);
-                      }}
-                    >
+                    <button>
                       <i class="fa fa-arrow-down"></i>
                     </button>
                   </div>
                 </div>
-                <div className="h3 col-8 my-auto">{answer.ansTitle}</div>
+                <div className="h3 col-8 my-auto"></div>
               </div>
               <div className="row">
-                <div className="my-1">{answer.answer}</div>
+                <div className="my-1"></div>
               </div>
             </div>
           );
-        })}
+        })} */}
       </div>
     </div>
   );
--- client/src/components/QuestionValue.js
+++ client/src/components/QuestionValue.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import QuestionPage from "./Question";
 import axios from "axios";
-import { Questions } from "../shared/Question";
 import { useParams } from "react-router-dom";
 export const QuestionValue = () => {
-  // const [question, Setquestion] = useState("");
-  // const GetQuestion = () => {
-  //   const { id } = useParams();
-  //   console.log(id);
-  //   axios
-  //     .get(`https://mapsmap.herokuapp.com/v1/api/question/?questionId=${id}`)
-  //     .then((response) => {
-  //       Setquestion(response.data);
-  //       console.log(response.data);
-  //     })
-  //     .catch((err) => console.log("Error is --->", err));
-  // };
-  // useEffect(() => {
-  //   GetQuestion();
-  // });
-  // console.log(question);
-  return <QuestionPage question={Questions[0]} />;
+  const [question, Setquestion] = useState("");
+  const { id } = useParams();
+  useEffect(() => {
+    axios
+      .get(`https://mapsmap.herokuapp.com/v1/api/question/?questionId=${id}`)
+      .then((response) => {
+        Setquestion(response.data);
+      })
+      .catch((err) => console.log("Error is --->", err));
+  }, []);
+  return <QuestionPage question={question} />;
 };
